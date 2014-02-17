243b085d6ee90e0536342cde63f5381a5324b7af
unknown <s.smirnov@SIT_PRG_2.spb.profitmed.ru>
2014-02-17 19:02:57 +0400

    Pass parameters to TestTax

--- Unit2/src/TestTax.java
+++ Unit2/src/TestTax.java
@@ -10,14 +10,38 @@ public class TestTax {
 		System.out.println("1 USD : ˆ" + Config.USD_EUR_RATE);
 		System.out.println("---------------------------------");
 
-		Tax tax1 = new Tax(65000, "TX", 4);
-		tax1.printTax();
+		switch (args.length) {
+		case 0:
+		case 1:
 
-		Tax tax2 = new Tax(30000, "NY", 4);
-		tax2.printTax();
+			Tax tax1 = new Tax(65000, "TX", 4);
+			tax1.printTax();
 
-		NJTax njTax = new NJTax(29000, 4);
-		njTax.printTax();
+			Tax tax2 = new Tax(30000, "NY", 4);
+			tax2.printTax();
+
+			NJTax njTax1 = new NJTax(29000, 4);
+			njTax1.printTax();
+			break;
+
+		case 2:
+
+			NJTax njTax = new NJTax(Double.parseDouble(args[0]),
+					Integer.parseInt(args[1]));
+			njTax.printTax();
+			break;
+
+		case 3:
+
+			Tax tax = new Tax(Double.parseDouble(args[0]), args[1],
+					Integer.parseInt(args[2]));
+			tax.printTax();
+			break;
+
+		default:
+			System.out.println("Bad parameters");
+			break;
+		}
 
 	}
 
