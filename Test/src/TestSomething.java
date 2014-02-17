1957ea43b3566698fc4bdae8aceac7affba99538
unknown <s.smirnov@SIT_PRG_2.spb.profitmed.ru>
2014-02-17 21:53:45 +0400

    Rename project Unit3 to Unit4

---
+++ Test/src/TestSomething.java
@@ -0,0 +1,22 @@
+
+public class TestSomething {
+
+	public static void main(String[] args) {
+		
+		
+		String c = "Natasha";
+		
+		String a = new String("Natasha");
+		String b = new String("Natasha");
+		
+		
+		if (a == b ) {
+			System.out.println("true");
+		}else{
+			System.out.println("false");
+		}
+			
+		
+	}
+
+}
