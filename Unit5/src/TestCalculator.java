2f8dc620fedcdc89fa54466d09166ba46e8293a0
unknown <s.smirnov@SIT_PRG_2.spb.profitmed.ru>
2014-03-13 21:28:34 +0400

    Start rewriting tasks in unit5

---
+++ Unit5/src/TestCalculator.java
@@ -0,0 +1,19 @@
+import com.practicaljava.unit5.Calculator;
+
+ 
+/**
+ * @author Svyatoslav Strygin <svyatoslav.strygin@gmail.com>
+ */
+public class TestCalculator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Calculator calculator = new Calculator();
+		
+
+	}
+
+}
--- Unit5/src/com/practicaljava/unit5/Calculator.java
+++ Unit5/src/com/practicaljava/unit5/Calculator.java
@@ -1,82 +1,52 @@
+/**
+ * 
+ */
 package com.practicaljava.unit5;
 
-import java.awt.BorderLayout;
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 /**
  * @author Svyatoslav Strygin <svyatoslav.strygin@gmail.com>
  */
 public class Calculator {
 	
+	private JFrame mainWindow;
 	private JPanel windowContent;
-	private JTextField displayField;
-	private JButton button0;
-	private JButton button1;
-	private JButton button2;
-	private JButton button3;
-	private JButton button4;
-	private JButton button5;
-	private JButton button6;
-	private JButton button7;
-	private JButton button8;
-	private JButton button9;
-	private JButton bottonPoint;
-	private JButton buttonEqual;
-	private JPanel panel;
+	private GridLayout gridLayout;
+	private JButton[] numberButtons;
+	
 	
 	
 	public Calculator(){
+	 
 		
 		windowContent = new JPanel();
+		gridLayout = new GridLayout(4,3);
 		
-		BorderLayout borderLayout = new BorderLayout();
-		windowContent.setLayout(borderLayout);
+		windowContent.setLayout(gridLayout);
 		
-		displayField = new JTextField(30);
-		windowContent.add("North", displayField);
 		
-		button0 = new JButton("0");
-		button1 = new JButton("1");
-		button2 = new JButton("2");
-		button3 = new JButton("3");
-		button4 = new JButton("4");
-		button5 = new JButton("5");
-		button6 = new JButton("6");
-		button7 = new JButton("7");
-		button8 = new JButton("8");
-		button9 = new JButton("9");
+		numberButtons = new JButton[10];
+		for (int i = 0; i < 10; i++) {
+			numberButtons[i] = new JButton(String.valueOf(i));
+			windowContent.add(numberButtons[i]);
+		}
 		
-		bottonPoint = new JButton(".");
-		buttonEqual = new JButton("=");
 		
-		panel = new JPanel();
+		mainWindow = new JFrame("javax.swing Calculator");
+		mainWindow.setContentPane(windowContent);
+		mainWindow.setSize(400,400);
+		mainWindow.setVisible(true);
 		
-		GridLayout gridLayout = new GridLayout(4,3);
-		panel.setLayout(gridLayout);
-		panel.add(button0);
-		panel.add(button1);
-		panel.add(button2);
-		panel.add(button3);
-		panel.add(button4);
-		panel.add(button5);
-		panel.add(button6);
-		panel.add(button7);
-		panel.add(button8);
-		panel.add(button9);
-		panel.add(bottonPoint);
-		panel.add(buttonEqual);
-		
-		windowContent.add("Center",panel);
-		
-		JFrame frame = new JFrame("Calculator");
-
-		frame.setContentPane(windowContent);
-		frame.pack();
-		frame.setVisible(true);
 	}
+	
+	
+	
+	
+	
+
 }
